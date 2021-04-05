93646f142dac15dd260d58987b8951d21518175d
Taniv Goyal <goyal.tanivji@gmail.com>
2021-04-05 18:38:00 +0530

    Final

--- src/Screens/Chart/Chart.js
+++ src/Screens/Chart/Chart.js
@@ -6,34 +6,16 @@ import fontFamily from '../../styles/fontFamily';
 import * as shape from 'd3-shape';
 export default class Chart extends Component {
   render() {
-    const fill = 'rgb(134, 65, 244)';
-    const data = [
-      50,
-      10,
-      40,
-      95,
-      -4,
-      -24,
-      null,
-      85,
-      undefined,
-      0,
-      35,
-      53,
-      -53,
-      24,
-      50,
-      -20,
-      -80,
-    ];
-    const randomColor = () => ('#' + ((Math.random() * 0xffffff) << 0).toString(16) + '000000').slice(0, 7)
+    const fill = 'rgb(129, 30, 144)';
+    const data = [20,30,50,75,-2,-32,null,75,undefined,1,0,23,null,24,20,-27,-70,];
+    const newColor = () => ('#' + ((Math.random() * 0xffffff) << 0).toString(16) + '000000').slice(0, 7)
  
-    const pieData = data
+    const pieChartData = data
         .filter((value) => value > 0)
         .map((value, index) => ({
             value,
             svg: {
-                fill: randomColor(),
+                fill: newColor(),
                 onPress: () => console.log('press', index),
             },
             key: `pie-${index}`,
@@ -41,51 +23,52 @@ export default class Chart extends Component {
 
     return (
        <ScrollView >
-       <Text style={styles.title}>BAR GRAPH</Text>
+       <Text style={styles.main}>BAR GRAPH</Text>
         <BarChart
-          style={styles.bargraph}
+          style={styles.bar}
           data={data}
           svg={{fill}}
           contentInset={{top: 30, bottom: 30}}>
           <Grid />
         </BarChart>
-        <Text style={styles.title}>AREA CHART</Text>
+        <Text style={styles.main}>AREA CHART</Text>
         <AreaChart
-          style={styles.bargraph}
+          style={styles.bar}
           data={data}
           contentInset={{top: 30, bottom: 30}}
           curve={shape.curveNatural}
           svg={{fill}}>
           <Grid />
         </AreaChart>
-        <Text style={styles.title}>LINE CHART</Text>
+        <Text style={styles.main}>LINE CHART</Text>
         <LineChart
-                style={styles.bargraph}
+                style={styles.bar}
                 data={data}
-                svg={{ stroke: 'rgb(134, 65, 244)' }}
+                svg={{ stroke: 'rgb(129, 30, 144)' }}
                 contentInset={{ top: 20, bottom: 20 }}
             >
                 <Grid />
             </LineChart>
-            <Text style={styles.title}>PIE CHART</Text>
-            <PieChart style={styles.bargraph} data={pieData} />
+            <Text style={styles.main}>PIE CHART</Text>
+            <PieChart style={styles.bar} data={pieChartData} />
        </ScrollView>
     );
   }
 }
 const styles = StyleSheet.create({
-  bargraph: {
+  bar: {
     height: 300,
     width: 300,
     marginHorizontal: 30,
+    // marginTop:30
   },
-  title: {
+  main: {
     textAlign: 'center',
-    color: colors.btnColor,
     fontFamily: fontFamily.regular,
     marginBottom:20,
-    marginTop: 20,
+    marginTop: 30,
     fontSize: 20,
+    color: colors.btnColor,
     
   },
  
